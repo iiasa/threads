11a593ea97e2e8c5c95e1f356bdcda7371be1d9a
S Rupesh <wrufesh@gmail.com>
2025-08-06 11:38:47 +0200

    add STATIC_ROOT in settings

--- accthreads/settings.py
+++ accthreads/settings.py
@@ -18,6 +18,8 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
